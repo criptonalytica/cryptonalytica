0c6073d7a84aab1e4106c633793c19f17fee58ee
ThalisonCosta <thalisoncosta123@gmail.com>
2022-05-17 10:48:28 -0400

    fixes on the table

--- components/Table.vue
+++ components/Table.vue
@@ -2,31 +2,38 @@
     <div>
         <table>
             <thead>
-                <th>#</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>24h</th>
-                <th>7 Days</th>
-                <th>Start</th>
-                <th>Market Cap</th>
-                <th>24h Volume</th>
-                <th>Circ. Supply</th>
-                <th>Weight</th>
+                <tr > <!--   se preferir pode fazer um v-for do título da tabela, só que a formatação ficou mais feia. não entendi o pq...       id="head" v-for="item in header" :key="item"--> 
+                    <!--  só tirar o comentário também desse campo e vai funfar <th @click="sort(item.title)">{{item.title}}</th> -->
+                    <th @click="sort('ranking')">#</th>
+                    <th @click="sort('name')" icon="arrow-down">Name</th> <!-- arrow-down quando tu adicionar o icon nos teus css maluco-->
+                    <th @click="sort('price')">Price</th>
+                    <th @click="sort('oneDay')">24h</th>
+                    <th @click="sort('sevenDays')">7 Days</th>
+                    <th @click="sort('start')">Start</th>
+                    <th @click="sort('market')">Market Cap</th>
+                    <th @click="sort('volume')">24h Volume</th>
+                    <th @click="sort('circ')">Circ. Supply</th>
+                    <th @click="sort('weight')">Weight</th>
+                </tr>
             </thead>
-            <tbody v-for="item in table.tbody" :key="item.ranking">
-                <td>{{item.ranking}}</td>
-                <td class="icon"> <img :src="item.icon"> </td>
-                <td>{{item.name}}</td>
-                <td>{{item.price}}</td>
-                <td>{{item.oneDay}}%</td>
-                <td>{{item.sevenDays}}%</td>
-                <td>{{item.star}}</td>
-                <td>{{item.market}}M</td>
-                <td>{{item.volume}}M</td>
-                <td>{{item.circ}}M</td>
-                <td>{{item.weight}}%</td>
+            <tbody>
+                <tr v-for="item in sortedData" :key="item.ranking">
+                    <td>{{item.ranking}}</td>
+                    <td icon="arrow-right"> <img :src="item.icon"> {{item.name}}</td>
+                    <td>{{item.price}}</td>
+                    <td v-if="item.oneDay>0" style="color:var(--color-green)">{{item.oneDay}}%</td>
+                    <td v-else style="color:var(--color-red)"> {{item.oneDay}} </td>
+                    <td v-if="item.sevenDays>0" style="color:var(--color-green)">{{item.sevenDays}}%</td>
+                    <td v-else style="color:var(--color-red)"> {{item.sevenDays}} </td>
+                    <td>{{item.start}}</td>
+                    <td>{{item.market}}M</td>
+                    <td>{{item.volume}}M</td>
+                    <td>{{item.circ}}M</td>
+                    <td>{{item.weight}}%</td>
+                </tr>
             </tbody>
         </table>
+        <!-- debug: sort={{currentSort}}, dir={{currentSortDir}} --- era só pra ver se tava mudando, deixa salvo aí vai que seja importante algum dia, ou não  -->
     </div>
 </template>
 
@@ -35,39 +42,69 @@ export default {
 
     data() {
         return {
-            table: {
                 tbody: [
-                    {ranking: 01,icon:'https://s2.coinmarketcap.com/static/img/coins/200x200/7501.png', name: 'Woo network', price: 118.00, oneDay: +2.32, sevenDays: +4.12, star:'Feb,24', market: 281, volume: 16, circ: 54, weight: 11},
-                    {ranking: 02, icon: 'https://avatars.githubusercontent.com/u/67821563?s=280&v=4', name: 'Anchor Cripto', price: 324.00, oneDay: -4.23, sevenDays: +4.12, star:'Jan,9', market: 21, volume: 14, circ: 23, weight: 81},
-                    {ranking: 03, icon: 'https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png', name: 'Solano Network', price: 543.00, oneDay: +2.98, sevenDays: +1.12, star:'Dec,4', market: 81, volume: 11, circ: 11, weight: 41},
-                    {ranking: 04, icon:'https://s2.coinmarketcap.com/static/img/coins/64x64/6756.png', name: 'Alaca Token', price: 232.00, oneDay: +3.6, sevenDays: +4.12, star:'Oct,18', market: 65, volume: 36, circ: 17, weight: 14},
-                    ]
+                    {ranking: '01', icon:'https://s2.coinmarketcap.com/static/img/coins/200x200/7501.png', name: 'Woo network', price: 118.00, oneDay: +2.32, sevenDays: -4.12, start:'Feb,24', market: 28, volume: 16, circ: 54, weight: 11},
+                    {ranking: '02', icon: 'https://avatars.githubusercontent.com/u/67821563?s=280&v=4', name: 'Anchor Cripto', price: 324.07, oneDay: -4.23, sevenDays: +4.12, start:'Jan,9', market: 21, volume: 14, circ: 23, weight: 81},
+                    {ranking: '03', icon: 'https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png', name: 'Solano Network', price: 543.10, oneDay: +2.98, sevenDays: -1.12, start:'Dec,4', market: 81, volume: 11, circ: 11, weight: 41},
+                    {ranking: '04', icon:'https://s2.coinmarketcap.com/static/img/coins/64x64/6756.png', name: 'Alaca Token', price: 232.98, oneDay: +3.6, sevenDays: +4.12, start:'Oct,18', market: 65, volume: 36, circ: 17, weight: 14},
+                    ],
+                // abaixo é pra se for usar o v-for dos títulos, se não quiser só apagar
+                header: [
+                    {title: '#'},{title:'name'},{title:'Price'},{title:'oneDay'},{title:'sevenDays'},{title:'start'},{title:'market'},{title:'volume'},{title:'circ'},{title:'weight'}
+                ],
+                currentSort:'name',
+                currentSortDir:'asc'
+
+        }
+    },
+
+    // daqui pra baixo não mexe que é mágica tmj :)
+    methods:{
+        sort:function(s) {
+            //if s == current sort, reverse
+            if(s === this.currentSort) {
+            this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
             }
+            this.currentSort = s;
+        }
+    },
+    computed: {
+        sortedData:function() {
+            return this.tbody.sort((a,b) => {
+                let modifier = 1;
+                if(this.currentSortDir === 'desc') modifier = -1;
+                if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+                if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+                return 0;
+            });
         }
     }
-
 }
+
+
 </script>
 
 <style lang="scss" scoped>
     table {
-        display: grid;
+        // display: grid;
         box-sizing: border-box;
         text-align: center;
         background-color:#fff;
         width: 1114px;
         height: 60px;
-        margin-bottom: 76px;
+        #head{
+            display:flex;
+            flex-direction:row;
+        }
+        tr {
+            text-align:center;
+        }
         th {
             // justify-content: space-between;
             // flex-direction: column;
             // align-items: flex-start;
             width: 1114px;
             height: 60px;
-        }
-        tbody{
-            // align-items: center;
-            // text-align: center;
         }
         td {
             width: 1114px;
@@ -78,6 +115,10 @@ export default {
             // flex-direction: row;
             // justify-content: space-between;
             // padding: 16px 0px;
+        }
+        .class {
+            display: flex;
+            flex-direction: column;
         }
         img {
             max-width: 28px;
