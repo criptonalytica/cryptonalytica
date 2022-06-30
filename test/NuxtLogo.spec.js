0ebaf13242366273ea05d50366674592ff0e4eb7
thalisonCosta <thalisoncosta123@gmail.com>
2022-06-29 21:17:58 -0400

    configuring to github pages

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -1,10 +1,6 @@
-// import { mount } from '@vue/test-utils'
-// import NuxtLogo from '@/components/NuxtLogo.vue'
 describe('NuxtLogo', () => {
   test('is a Vue instance', () => {
     const number = 2
     expect(number).toBe(2)
-    // const wrapper = mount(NuxtLogo)
-    // expect(wrapper.vm).toBeTruthy()
   })
 })
